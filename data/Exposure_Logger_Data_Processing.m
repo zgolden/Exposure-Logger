84df31a22a760de4f93f848eb9bb06e3297dfc1d
Zachary Golden <zgolden@pdx.edu>
2022-06-06 17:03:38 -0700

    Organized, Streamlined Code, Added Data

--- data/Exposure_Logger_Data_Processing.m
+++ data/Exposure_Logger_Data_Processing.m
@@ -6,31 +6,31 @@
 clear, clc
 
 %- Import wizard: Make sure data file is in same directory
-data = uiimport('5272022-night.TXT'); %replace file name
-
+dataimport = uiimport('EEM6042022.TXT'); %replace file name
+data = dataimport.EEM6042022; %- make sure to change to your file name after dot
 
 %- Creating a time vector that is the same length of the data to plot with
-time = 5*(1:length(data.data(:,1)));
+time = 5*(1:length(data(:,1)));
     % Data was sampled every five minutes, hence the 5
 
 %- Creating vectors to plot exposure limits on
-shortTermPM2p5 = 35*ones(length(data.data(:,1)),1);
-longTermPM2p5 = 12*ones(length(data.data(:,1)),1);
+shortTermPM2p5 = 35*ones(length(data(:,1)),1);
+longTermPM2p5 = 12*ones(length(data(:,1)),1);
 
-easLow = 300*ones(length(data.data(:,1)),1);
-easAccpt = 500*ones(length(data.data(:,1)),1);
-easMarg = 1000*ones(length(data.data(:,1)),1);
-easHigh = 3000*ones(length(data.data(:,1)),1);
+easLow = 300*ones(length(data(:,1)),1);
+easAccpt = 500*ones(length(data(:,1)),1);
+easMarg = 1000*ones(length(data(:,1)),1);
+easHigh = 3000*ones(length(data(:,1)),1);
 
 %- Creating vectors for the bar graph
-totalparticles = sum(data.data);
+totalparticles = sum(data);
 bary = totalparticles(:,7:10);
 barx = {'Particles > 0.3 \mu m/ 0.1L of Air', 'Particles > 0.5 \mu m/ 0.1L of Air', 'Particles > 1.0 \mu m/ 0.1L of Air', 'Particles > 2.5 \mu m/ 0.1L of Air'}
 
 
 % Plot 1 PM2.5 Standard Concentration Units
 figure(1)
-plot(time,data.data(:,2),time,shortTermPM2p5,time,longTermPM2p5)
+plot(time,data(:,2),time,shortTermPM2p5,time,longTermPM2p5)
 legend('Collected Data', 'EPA Short Term Exposure Limit (35 \mu g/m^3)', 'EPA Long Term Exposure Limit (12 \mu g/m^3)')
 xlabel('Time (min)')
 ylabel('Concentration (\mu g/m^3)')
@@ -41,7 +41,7 @@ grid off
 
 % Plot 2 PM2.5 Enviromental Concentration under atmospheric conditions
 figure(2)
-plot(time,data.data(:,5))
+plot(time,data(:,5))
 hold on
 plot(time, shortTermPM2p5)
 plot(time, longTermPM2p5)
@@ -54,7 +54,7 @@ hold off
 
 % Plot 3 eCO2 Readings from SGP30
 figure(3)
-plot(time,data.data(:,14))
+plot(time,data(:,14))
 hold on
 xlabel('Time (min)')
 ylabel('Concentration (ppm)')
@@ -65,7 +65,7 @@ hold off
 % Plot 4 TVOC Readings from SGP30
 figure(4)
 hold on
-plot(time,4.5*data.data(:,13)) %4.5 converts from ppb to ug/m^3
+plot(time,4.5*data(:,13)) %4.5 converts from ppb to ug/m^3
 plot(time,easLow,'g')
 plot(time,easAccpt,'y')
 plot(time,easMarg,'r')
